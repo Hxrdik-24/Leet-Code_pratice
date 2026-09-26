88a06f53336c3c10a7d861655e8017009c4ce216
Hardik__24 <hardikprajapati242008@gmail.com>
2026-09-26 21:18:35 +0530

    Time: 6 ms (44.41%), Space: 44.4 MB (65.83%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -3,17 +3,11 @@ class Solution {
         if(s.length() != t.length())
             return false;
         int[] fq1  = new int[26];
-        // count the all char of String 1
+        // count the all char of String 1 and inserat in array
         for(int i = 0; i < s.length(); i++)
         {
-            int n = s.charAt(i) - 'a';
-            fq1[n]++;
-        }
-
-        for(int i = 0; i < t.length(); i++)
-        {
-            int n = t.charAt(i) - 'a';
-            fq1[n]--;
+            fq1[s.charAt(i) - 'a']++;
+            fq1[t.charAt(i) - 'a']--;
         }
 
         for(int i = 0; i < 26; i++) 
